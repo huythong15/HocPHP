0483d87c04171d4c1ae417bdaa96d6e536e75fc9
huythong <peacehihi@gmail.com>
2017-08-20 20:34:35 +0700

    hehe
    12

---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+echo "<p align='center'><font color='blue' size='25px'>Ví dụ lệnh echo trong PHP!</font></p>";
+//hoặc
+print "<p align='center'><font color='red' size='25px'>Ví dụ lệnh print trong PHP!</font></p>";
+?>
+
